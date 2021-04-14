20f4c92a670a0295e627f0f7b7639d935a9c5912
Norbert Krupa <toftnqr@gmail.com>
2021-04-14 14:39:23 +0200

    Updates

---
+++ precache-manifest.c8a00336870641c100958f09575f1b7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970beb3fb6e9ca2a15945d5c4f149723",
+    "url": "/TetrisReactJS/index.html"
+  },
+  {
+    "revision": "3f837dd83ee53bd9d629",
+    "url": "/TetrisReactJS/static/css/main.b09ae088.chunk.css"
+  },
+  {
+    "revision": "db612d50647c4686fdd3",
+    "url": "/TetrisReactJS/static/js/2.8ed084ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TetrisReactJS/static/js/2.8ed084ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f837dd83ee53bd9d629",
+    "url": "/TetrisReactJS/static/js/main.4d2caf98.chunk.js"
+  },
+  {
+    "revision": "1a62a05c83eaa42eefb3",
+    "url": "/TetrisReactJS/static/js/runtime-main.79caf62e.js"
+  },
+  {
+    "revision": "66b7fbc4587243b7093542563327766f",
+    "url": "/TetrisReactJS/static/media/MuseoModerno-Medium.66b7fbc4.ttf"
+  },
+  {
+    "revision": "2428afe68123a99ad3c10d1bb260b2ed",
+    "url": "/TetrisReactJS/static/media/bg.2428afe6.png"
+  }
+]);
